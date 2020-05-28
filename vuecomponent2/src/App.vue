2842a710797591d06230fa9b3e638d8830cec531
hq <445414088@qq.com>
2020-05-28 09:32:12 +0800

    vue组件开发项目

---
+++ vuecomponent2/src/App.vue
@@ -0,0 +1,53 @@
+<template>
+<div>
+    <div>
+        <header class="site-header jumbotron">
+        <div class="container">
+            <div class="row">
+            <div class="col-xs-12">
+                <h1>请发表对React的评论</h1>
+            </div>
+            </div>
+        </div>
+        </header>
+        <div class="container">
+            <Add :addC="addC"/>
+            <List :comments="comments" :deleteC="deleteC"/>
+        </div>
+    </div>
+</div>
+ 
+</template>
+
+<script type="text/ecmascript-6">
+import List from "./component/List.vue";
+import Add from "./component/Add.vue"
+export default {
+    components:{
+        Add,
+        List
+    },
+    data(){
+        return {
+            comments:[
+                {id:1,username:"小脑斧",comment:"我在哪？？"},
+                {id:2,username:"小猫咪",comment:"我是谁？？"},
+                {id:3,username:"小老鼠",comment:"我要去哪？？"}
+            ]
+        }
+    },
+    methods:{
+        addC(obj){
+            this.comments.unshift(obj)
+        },
+        deleteC(index){
+            this.comments.splice(index,1);
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+
+</style>
---
+++ vuecomponent2/src/component/Add.vue
@@ -0,0 +1,50 @@
+<template>
+ <div class="col-md-4">
+        <form class="form-horizontal">
+          <div class="form-group">
+            <label>用户名</label>
+            <input type="text" class="form-control" placeholder="用户名" v-model="username">
+          </div>
+          <div class="form-group">
+            <label>评论内容</label>
+            <textarea class="form-control" rows="6" placeholder="评论内容" v-model="comment"></textarea>
+          </div>
+          <div class="form-group">
+            <div class="col-sm-offset-2 col-sm-10">
+              <button type="button" class="btn btn-default pull-right" @click="sub">提交</button>
+            </div>
+          </div>
+        </form>
+    </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+  props:["addC"],
+  data(){
+    return {
+      username:"",
+      comment:"",
+      id:Date.now()
+    }
+  },
+  methods:{
+    sub(){
+      let username = this.username;
+      let comment = this.comment;
+      let id = this.id;
+      if(username.trim() && comment.trim()){
+        let obj = {id,username,comment};
+        this.addC(obj)
+      }
+      this.username = "";
+      this.comment = "";
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+
+</style>
